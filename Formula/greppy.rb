ebf184cd79d89c14bc5c6206077e1c7bc82bf7ff
Dylan <burtond@uw.edu>
2026-01-07 01:21:31 -0800

    Add greppy formula

---
+++ Formula/greppy.rb
@@ -0,0 +1,20 @@
+class Greppy < Formula
+  include Language::Python::Virtualenv
+
+  desc "Semantic code search CLI using ChromaDB + Ollama"
+  homepage "https://github.com/dyoburon/greppy"
+  url "https://github.com/dyoburon/greppy/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "5c8a43d4a7f981cb4403e0b534586f22526ed1b87b6aec26aba93811f1e7930f"
+  license "MIT"
+
+  depends_on "python@3.11"
+
+  def install
+    virtualenv_create(libexec, "python3.11")
+    system libexec/"bin/pip", "install", "."
+  end
+
+  test do
+    system bin/"greppy", "--help"
+  end
+end
