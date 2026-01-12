33b7dbfd8a5b2e6a197862cb4fa849bcf94eaec1
Dylan <burtond@uw.edu>
2026-01-12 15:35:06 -0800

    greppy 0.3.1: add watch command

--- Formula/greppy.rb
+++ Formula/greppy.rb
@@ -3,8 +3,8 @@ class Greppy < Formula
 
   desc "Semantic code search CLI using ChromaDB + Ollama"
   homepage "https://github.com/dyoburon/greppy"
-  url "https://github.com/dyoburon/greppy/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "3138b6b400c7f98b03e050c1f9a85d659f62302f9db3c1103733f2e1f29e8564"
+  url "https://github.com/dyoburon/greppy/archive/refs/tags/v0.3.1.tar.gz"
+  sha256 "7d9c7670001d780c34a0b665e1c0a5d5aa7e679ead2d22f31ae909996bfae0c5"
   license "MIT"
 
   depends_on "python@3.11"
