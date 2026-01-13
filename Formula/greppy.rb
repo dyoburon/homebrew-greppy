9a7fff5ea2643d55830e21e6ac60bb8addd77163
Dylan <burtond@uw.edu>
2026-01-13 09:58:35 -0800

    Bump to v0.5.1 - add einops

--- Formula/greppy.rb
+++ Formula/greppy.rb
@@ -3,8 +3,8 @@ class Greppy < Formula
 
   desc "Semantic code search CLI using ChromaDB + CodeRankEmbed"
   homepage "https://github.com/dyoburon/greppy"
-  url "https://github.com/dyoburon/greppy/archive/refs/tags/v0.5.0.tar.gz"
-  sha256 "f3fb854687a0894544611621aad86351e39f376b4c5e24639723222c53eaf5f1"
+  url "https://github.com/dyoburon/greppy/archive/refs/tags/v0.5.1.tar.gz"
+  sha256 "3a5732b18869909c091677106058db2bc41f7bb698f235466aa3fe8449e3242f"
   license "MIT"
 
   depends_on "python@3.12"
