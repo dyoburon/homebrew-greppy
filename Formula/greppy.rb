ed9e1bbbf8f2695f77dc9951b4fc6b06cdcd2cf2
Dylan <burtond@uw.edu>
2026-01-13 09:46:21 -0800

    Bump to v0.5.0 - CodeRankEmbed + Python 3.12

--- Formula/greppy.rb
+++ Formula/greppy.rb
@@ -1,16 +1,16 @@
 class Greppy < Formula
   include Language::Python::Virtualenv
 
-  desc "Semantic code search CLI using ChromaDB + Ollama"
+  desc "Semantic code search CLI using ChromaDB + CodeRankEmbed"
   homepage "https://github.com/dyoburon/greppy"
-  url "https://github.com/dyoburon/greppy/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "6aaa2d760ef3d6cb44726471ba459b67f47ea764766e40045b7321e79290310d"
+  url "https://github.com/dyoburon/greppy/archive/refs/tags/v0.5.0.tar.gz"
+  sha256 "f3fb854687a0894544611621aad86351e39f376b4c5e24639723222c53eaf5f1"
   license "MIT"
 
-  depends_on "python@3.11"
+  depends_on "python@3.12"
 
   def install
-    python3 = Formula["python@3.11"].opt_bin/"python3.11"
+    python3 = Formula["python@3.12"].opt_bin/"python3.12"
     system python3, "-m", "venv", libexec
     system libexec/"bin/pip", "install", "--upgrade", "pip"
     system libexec/"bin/pip", "install", buildpath
